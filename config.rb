53fb4f81bf36c2a4c4756736e3246ce75a4de017
Bridget Almas <balmsa@gmail.com>
2017-10-09 19:34:49 -0400

    reorg and new pages

--- config.rb
+++ config.rb
@@ -3,6 +3,8 @@ set :js_dir, 'js'
 set :fonts_dir, 'fonts'
 activate :bh
 activate :sprockets
+activate :directory_indexes
+page "/content/*", :layout => "content"
 sprockets.append_path 'bower_components/bootstrap-sass/assets/fonts'
 sprockets.append_path 'bower_components'
 configure :build do
