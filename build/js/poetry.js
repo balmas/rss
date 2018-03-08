840fb2a70e177fe3c6fab9975a2c615052a038f6
Bridget Almas <balmsa@gmail.com>
2018-03-08 16:49:22 -0500

    add poetry prototype page

---
+++ build/js/poetry.js
@@ -0,0 +1,58 @@
+let currentAudioEnd = {};
+function endSegment() {
+  currentAudioEnd[this.id] = this.duration
+  document.querySelectorAll('.poetry.text').forEach((e) => e.classList.remove('active'))
+}
+function playSegment() {
+  let tElem = document.getElementById(this.dataset.track)
+  let audio = tElem.parentElement
+  audio.currentTime = this.dataset.start
+  currentAudioEnd[audio.id] = this.dataset.end
+  audio.play()
+  console.log("Move carousel to " + this.dataset.image)
+  $(`#${audio.id}-carousel`).carousel(parseInt(this.dataset.image))
+}
+
+function loadCues() {
+  let track = this.track
+  for (let j = 0; j < track.cues.length; ++j) {
+    let cue = track.cues[j]
+    let text = document.createElement('span')
+      text.classList.add('poetry')
+      text.classList.add('text')
+      text.setAttribute('id',`track-${track.id}-cue-${cue.id}`)
+      text.setAttribute('data-start',cue.startTime)
+      text.setAttribute('data-end',cue.endTime)
+      text.setAttribute('data-track',track.id)
+      text.setAttribute('data-image',cue.id-1)
+      text.innerHTML = cue.text
+      document.getElementById("current-transcript").appendChild(text)
+      text.addEventListener("click", playSegment, false)
+    }
+}
+function watchCues() {
+  for (let c of this.track.activeCues) {
+    document.querySelectorAll('.poetry.text').forEach((e) => e.classList.remove('active'))
+    document.getElementById(`track-${this.track.id}-cue-${c.id}`).classList.add('active')
+  }
+}
+function setupAudio() {
+  document.querySelectorAll('audio').forEach((a) => {
+    currentAudioEnd[a.id] = a.duration
+    a.addEventListener('timeupdate',pauseAudio, false)
+    a.addEventListener('ended',endSegment,false)
+    a.addEventListener('pause',endSegment,false)
+  })
+  document.querySelectorAll('track').forEach((t) => {
+    t.addEventListener("load", loadCues, false)
+    t.addEventListener("cuechange", watchCues, false)
+  })
+}
+
+function pauseAudio() {
+  if (this.currentTime > currentAudioEnd[this.id]) {
+    this.pause()
+  }
+}
+setupAudio()
+;
