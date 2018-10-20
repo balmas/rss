9656be9244ec670559d403fdc1d81f7306d92ce4
Bridget Almas <balmsa@gmail.com>
2018-10-20 17:31:07 -0400

    initial commit

--- config.rb
+++ config.rb
@@ -5,7 +5,6 @@ activate :bh
 activate :sprockets
 activate :directory_indexes
 activate :asset_hash
-page "/pages/poetry/*", :layout => "text"
 page "/pages/*", :layout => "content"
 sprockets.append_path 'bower_components/bootstrap-sass/assets/fonts'
 sprockets.append_path 'bower_components'
@@ -16,7 +15,4 @@ configure :development do
 end
 configure :build do
   ignore "*.less"
-  activate :google_analytics do |ga|
-    ga.tracking_id = 'UA-8210342-1'
-  end
 end
